488b3779172d61a74df1c9d801c5ed1bc68ee7d8
Daniel T. Schmitt <daniel.t.schmitt@gmx.net>
2020-04-23 16:41:54 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pypbo',
+    version='0.1',
+    packages=['pypbo', 'pypbo.perf'],
+    url='https://github.com/nocluebutalotofit/pypbo',
+    license='',
+    author='esvhd, binrush, nocluebutalotofit',
+    author_email='',
+    description='Python implmenetation of Probability of Backtest Overfitting[1].'
+                '[1] Bailey, David H. and Borwein, Jonathan M. and Lopez de Prado,'
+                ' Marcos and Zhu, Qiji Jim, The Probability of Backtest Overfitting'
+                ' (February 27, 2015). Journal of Computational Finance (Risk Journals), '
+                '2015, Forthcoming. Available at SSRN: http://ssrn.com/abstract=2326253 or '
+                'http://dx.doi.org/10.2139/ssrn.2326253'
+)
